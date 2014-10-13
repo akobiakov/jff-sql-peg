2447905e0d724a977decb1ef0f45b7eb4d5b3683
Alexey Kobyakov <kobyakov@outlook.com>
2014-10-13 21:37:01 +0400

    Naming utils (now tested)

--- src/main/scala/ru/mrstrictly/jff/bnf/util/Naming.scala
+++ src/main/scala/ru/mrstrictly/jff/bnf/util/Naming.scala
@@ -2,11 +2,13 @@ package ru.mrstrictly.jff.bnf.util
 
 object Naming {
   val toValidJavaMethodName: PartialFunction[String, String] = {
-    case anyString: String if anyString != null && anyString.length > 0 =>
-      if (Character.isJavaIdentifierStart(anyString.charAt(0))) {
-        anyString.charAt(0).toLower + toValidRestOfJavaMethodName(anyString.substring(1).toLowerCase)
+    case anyString: String if anyString != null && anyString.trim.length > 0 =>
+      val firstChar = anyString.trim.charAt(0)
+      val restOfString = anyString.trim.substring(1)
+      if (Character.isJavaIdentifierStart(firstChar)) {
+        firstChar.toLower + toValidRestOfJavaMethodName(restOfString)
       } else {
-        '_' + toValidRestOfJavaMethodName(anyString.toLowerCase)
+        toValidJavaMethodName(restOfString)
       }
   }
 
@@ -14,9 +16,9 @@ object Naming {
     if (anyString.length == 0) return ""
     val firstChar = anyString.charAt(0)
     if (Character.isJavaIdentifierPart(firstChar)) {
-      firstChar + toValidRestOfJavaMethodName(anyString.substring(1).toLowerCase)
+      firstChar + toValidRestOfJavaMethodName(anyString.substring(1))
     } else {
-      toValidRestOfJavaMethodName(anyString.substring(1).toUpperCase)
+      toValidRestOfJavaMethodName(anyString.substring(1).capitalize)
     }
   }
 }
--- src/test/scala/ru/mrstrictly/jff/bnf/util/Naming$Test.scala
+++ src/test/scala/ru/mrstrictly/jff/bnf/util/Naming$Test.scala
@@ -17,5 +17,75 @@ class Naming$Test extends FlatSpec with Matchers {
     }
   }
 
+  it should "produce MatchError when an input is one space" in {
+    intercept[MatchError] {
+      Naming.toValidJavaMethodName(" ")
+    }
+  }
 
+  it should "produce MatchError when an input is just spaces" in {
+    intercept[MatchError] {
+      Naming.toValidJavaMethodName("      ")
+    }
+  }
+
+  "A string 'toValidJavaMethodName'" must "remains the same" in {
+    assertResult("toValidJavaMethodName") {
+      Naming.toValidJavaMethodName("toValidJavaMethodName")
+    }
+  }
+
+  "A string 'a'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("a")
+    }
+  }
+
+  "A string 'A'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("A")
+    }
+  }
+
+  "A string '   a'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("   a")
+    }
+  }
+
+  "A string '  A '" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("  A ")
+    }
+  }
+
+  "A string '-a'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("-a")
+    }
+  }
+
+  "A string '--a'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("--a")
+    }
+  }
+
+  "A string 'a-'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("a-")
+    }
+  }
+
+  "A string 'A--'" must "be converted to 'a'" in {
+    assertResult("a") {
+      Naming.toValidJavaMethodName("A--")
+    }
+  }
+
+  "A string 'full-of-hyphens and spaces'" must "be converted to 'fullOfHyphensAndSpaces'" in {
+    assertResult("fullOfHyphensAndSpaces") {
+      Naming.toValidJavaMethodName("full-of-hyphens and spaces")
+    }
+  }
 }
